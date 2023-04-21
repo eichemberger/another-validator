b2320b0714c774a60c0f0d2003006c7df1603b13
German Eichemberger <germaneichemberger@gmail.com>
2023-04-21 10:16:20 -0300

    adds import to index file

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { BaseValidator } from './validators/BaseValidator';
 export { NumberValidator } from './validators/NumberValidator';
 export { SchemaValidator } from './validators/SchemaValidator';
 export { Validator } from './validators/Validator';
+export { CardValidator } from './validators/CardValidator';
