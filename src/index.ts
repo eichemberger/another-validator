7f4b654fba8b0b5b0c14dd92b44ae72474640e6d
German Eichemberger <germaneichemberger@gmail.com>
2023-04-23 09:41:50 -0300

    adds imports

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export { SchemaValidator } from "./validators/SchemaValidator";
 export { Validator } from "./validators/Validator";
 export { CardValidator } from "./validators/CardValidator";
 export { CardProvider } from "./constants/CardProvider";
+export { ValidationError } from "./errors/ValidationError";
+export { RuleFunction } from "./types/CustomValidator";
