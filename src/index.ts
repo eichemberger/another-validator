53c3d352184cdfad6bb9232dcb174f98d9375a09
German Eichemberger <germaneichemberger@gmail.com>
2023-04-25 10:10:21 -0300

    adds imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-export { ArrayValidator } from "./validators/ArrayValidator";
-export { BaseValidator } from "./validators/BaseValidator";
-export { NumberValidator } from "./validators/NumberValidator";
-export { SchemaValidator } from "./validators/SchemaValidator";
 export { Validator } from "./validators/Validator";
-export { CardValidator } from "./validators/CardValidator";
 export { CardProvider } from "./constants/CardProvider";
+export { BaseValidator } from "./validators/BaseValidator";
+export { CardValidator } from "./validators/CardValidator";
 export { ValidationError } from "./errors/ValidationError";
 export { CreditCardError } from "./errors/CreditCardError";
+export { ArrayValidator } from "./validators/ArrayValidator";
+export { NumberValidator } from "./validators/NumberValidator";
+export { SchemaValidator } from "./validators/SchemaValidator";
+
+export * from "./functions/validations";
+export * from "./functions/creditCardValidations";
